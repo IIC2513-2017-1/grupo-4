67899618629bdaddd31eafd5505fb9fab0dd4efa
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2017-04-27 16:04:27 -0300

    Add User RoR validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+    # Manejar de la misma manera tEST@example.com y test@example.com
+    before_save { self.email = email.downcase }
+
+    # Declaración de roles
+    enum role: [:ADMIN, :USER]
+    validates :role, presence: true
+
+    # Validaciones de Rails
+    validates :email, presence: true,
+        format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+        uniqueness: { case_sensitive: false }
+    
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
+    # Contraseñas
+    has_secure_password
+    validates :password, presence: true
 end
