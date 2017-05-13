2f4db56aa1e0d3f086a6bdc45ce8e11ef3fd1b34
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2017-05-13 02:26:13 -0300

    Add Product dependency

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
     # Relaciones
     belongs_to :category
     validates :category, presence: true
-    has_many :shopping_cart_products
+    has_many :shopping_cart_products, dependent: :destroy
     has_many :shopping_carts, through: :shopping_cart_products
 
     # Validaciones de Rails
